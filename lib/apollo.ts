e94cc2c4d2a01bedd31a1b745545ac5af0ccae21
purps <goglqbmo@gmail.com>
2021-12-17 17:15:45 +0300

    removed logs of api_url and addded logout link

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,8 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-console.log(API_URL)
-
 const apolloClient = new ApolloClient({
   uri: `${API_URL}/graphql`,
   cache: new InMemoryCache(),
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,6 @@
 //import { NextPage } from 'next'
 import { useUser } from '@auth0/nextjs-auth0'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const ProfilePage = () => {
@@ -16,7 +17,13 @@ const ProfilePage = () => {
           <img className="" src={`${user.picture}`} alt="" />
           <h1 className="text-xl font-bold">{user.name}</h1>
         </div>
-        <div className="rounded-lg p-5 bg-fashop-2"></div>
+        <div className="rounded-lg p-5 bg-fashop-2">
+          <Link href="/api/auth/logout">
+            <a className="px-5 py-2 rounded-full bg-fashop-3 text-fashop-primary font-medium">
+              logout
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   ) : (
