acb19c719582105b01a63418b7c6c73f8e7c0295
purps <goglqbmo@gmail.com>
2021-12-17 17:06:14 +0300

    log API_URL value

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,6 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
+console.log(API_URL)
+
 const apolloClient = new ApolloClient({
   uri: `${API_URL}/graphql`,
   cache: new InMemoryCache(),
